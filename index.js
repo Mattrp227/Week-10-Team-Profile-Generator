88c8fca639dd85321c0b8ad578df6b05b0154fcb
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-09-21 19:51:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+//Create variable index.js that link to other files
 
+const thisTeam = [];
+const inquirer = require ('inquirer');
+const fs = require ('fs');
+const Manager = require
+const Engineer = require
+const Intern = require
