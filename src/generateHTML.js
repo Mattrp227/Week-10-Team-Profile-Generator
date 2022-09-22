d5c912472426ca66e059e150e39bf82028a3706c
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-09-21 23:31:52 -0400

    Update generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,1 +1,38 @@
+//create manager for html
 
+const beginManager = function (manager) {
+    return `
+    <div class = "MANAGER">
+    <div class = "header">
+    <h1>${manager.name}</h1>
+    </div>
+    <div class = "manager info">
+    <p class = "employeeid"> ID Number: ${manager.employeeid}</p>
+    <p class = "emailaddress"> Email Address: ${manager.emailadress}</p>
+    <p class = "officelocationnumber"> Office Number: ${manager.officelocationnumber}</p>
+    </div>
+    </div>
+    
+    
+    `
+}
+
+
+//create employee
+const promptEmployees = function (employee) {
+    return `
+    <div class = "EMPLOYEE">
+    <div class = "header">
+    <h1>${employee.name}</h1>
+    </div>
+    <div class="manager info">\
+    <p class = "role"> role: ${employee.role}</p>
+    <p class = "employeeid"> ID Number: ${employee.employeeid}</p>
+    <p class = "emailaddress"> Email Address: ${employee.emailadress}</p>
+    <p class = "officelocationnumber"> Office Number: ${employee.officelocationnumber}</p>
+    </div>
+    </div>
+    
+    
+    `
+}
