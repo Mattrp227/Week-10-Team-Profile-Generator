fdb55356427eeaf000ebb3c61c052fdd73ed6bb5
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-09-21 23:57:54 -0400

    Update Intern.test.js

--- Tests/Intern.test.js
+++ Tests/Intern.test.js
@@ -1,1 +1,16 @@
+//need to use employee constructor again
 
+const Intern = require('../lib/Employee');
+const Intern = require('../lib/Intern');
+
+//test getting the employee info
+
+test('get employee info', () => {
+    const intern = new Intern ('Bob', 250, 'xxxx@gmail.com', 'Intern');
+
+
+    expect(intern.name).toEqual(expect.any(String));
+    expect(intern.employeeid).toEqual(expect.any(Number));
+    expect(intern.emailaddress).toEqual(expect.any(String));
+    expect(intern.role).toEqual(expect.any('Intern'));
+});
