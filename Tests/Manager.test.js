3365e961733a3443cd50a33691723ee6bd51002c
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-09-21 23:58:10 -0400

    Update Manager.test.js

--- Tests/Manager.test.js
+++ Tests/Manager.test.js
@@ -1,1 +1,16 @@
+//need to use employee constructor again
 
+const manager = require('../lib/Employee');
+const Manager = require('../lib/Manager');
+
+
+//test getting the manager's info
+
+test('get manager info', () => {
+    const manager = new Manager ('Bob', 250, 'xxxx@gmail.com');
+
+
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.employeeid).toEqual(expect.any(Number));
+    expect(manager.emailaddress).toEqual(expect.any(String));
+});
